210a5209145fd318e56919d29915adf8eef96906
PranavDeepakSathya <sathya.pranav.deepak@gmail.com>
2026-04-07 03:01:55 +0000

    really_pathetic_flops_though

--- basic_kernel/binding.cpp
+++ basic_kernel/binding.cpp
@@ -2,11 +2,8 @@
 
 extern "C" void launch_matmul(void* A, void* B, void* C);
 
-torch::Tensor matmul_hip(torch::Tensor A, torch::Tensor B) {
-  int M = 4096, N = 4096;
-  auto C = torch::zeros({M * N}, torch::dtype(torch::kFloat32).device(A.device()));
+void matmul_hip(torch::Tensor A, torch::Tensor B, torch::Tensor C) {
   launch_matmul(A.data_ptr(), B.data_ptr(), C.data_ptr());
-  return C;
 }
 
 PYBIND11_MODULE(hip_matmul, m) {
